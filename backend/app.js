1942dc6d0f7279e6d7b2c9613fc50b02d4635dcc
yfqiao <qiao.yif@northeastern.edu>
2022-07-05 00:04:56 -0700

    uodate

--- backend/app.js
+++ backend/app.js
@@ -45,6 +45,24 @@ router.use('/api/posts', posts)
 //Configure router
 app.use(router.routes()).use(router.allowedMethods())
 
+app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*')
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE')
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true)
+
+  // Pass to next layer of middleware
+  next()
+})
+
 //-------- deployment----------
 
 const port = process.env.PORT || 8080
